d52016f4d2a4a110a2ea88ef7876f12946e1db8b
ysqyang <v-yangqi@microsoft.com>
2020-09-22 14:56:05 +0800

    removed hardcode in multi_process_launcher

--- examples/ecr/dqn/multi_process_launcher.py
+++ examples/ecr/dqn/multi_process_launcher.py
@@ -5,16 +5,23 @@
 This script is used to debug distributed algorithm in single host multi-process mode.
 """
 
+import io
 import os
+import yaml
 
-ACTOR_NUM = 1   # must be same as in config
-LEARNER_NUM = 1
+from maro.utils import convert_dottable
+
+
+with io.open("config.yml", "r") as in_file:
+    raw_config = yaml.safe_load(in_file)
+    config = convert_dottable(raw_config)
+
 
 learner_path = "components/dist_learner.py &"
 actor_path = "components/dist_actor.py &"
 
-for l_num in range(LEARNER_NUM):
+for l_num in range(config.distributed.learner.peer["actor_worker"]):
     os.system(f"python " + learner_path)
 
-for a_num in range(ACTOR_NUM):
+for a_num in range(config.distributed.actor.peer["actor"]):
     os.system(f"python " + actor_path)
